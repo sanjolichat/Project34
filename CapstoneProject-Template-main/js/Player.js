0e30e580a9059afdd42c4e5bd3e526fc4e76d083
sanjolichat <97324282+sanjolichat@users.noreply.github.com>
2022-05-02 23:39:14 -0700

    Add files via upload

---
+++ CapstoneProject-Template-main/js/Player.js
@@ -0,0 +1,53 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    //this.image = loadImage("./assets/player.png");
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+
+  displayAlternate() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    this.image = loadImage("./assets/axguy2.png");
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+
+  displayOriginal() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    this.image = loadImage("./assets/player.png");
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
+
+
